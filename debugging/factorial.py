70358c6be55b8146baf8065f0299bdbce1b873f0
Jackpot-007 <jackpotmerlon@gmail.com>
2025-09-02 23:54:45 +0000

    factorial

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -7,7 +7,7 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1   # 🔹 decremento para evitar bucle infinito
+        n -= 1
     return result
 
 if len(sys.argv) < 2:
